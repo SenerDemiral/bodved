8a8ee443812bc02815954147fb91203658bab411
SenerDemiral <sener.demiral@gmail.com>
2017-10-17 14:29:38 +0300

    171017

--- bodved/ppMacPage.json.cs
+++ bodved/ppMacPage.json.cs
@@ -20,14 +20,15 @@ namespace bodved
 
                 sng = Singles.Add();
                 sng.Rakip = r.PPAd;
-                sng.Sonuc = $"[{k.SW}{r.SW}] {k.S1W}-{r.S1W} {k.S2W}-{r.S2W} {k.S3W}-{r.S3W} {k.S4W}-{r.S4W} {k.S5W}-{r.S5W}";
+                sng.Sonuc = $"[{k.SW}-{r.SW}] {k.S1W}-{r.S1W} {k.S2W}-{r.S2W} {k.S3W}-{r.S3W} {k.S4W}-{r.S4W} {k.S5W}-{r.S5W}";
 
+                // Esit sonuc sadece 0-0 da olur, bu da oynanmamis demektir
                 if (k.SW > r.SW)
                 {
                     TMSW++;
                     TSSW += k.SW;
                 }
-                else
+                else if (k.SW < r.SW)
                 {
                     TMSL++;
                     TSSL += r.SW;
@@ -50,14 +51,14 @@ namespace bodved
                 dbl.Rakip1 = r[0].PPAd;
                 dbl.Rakip2 = r[1].PPAd;
 
-                dbl.Sonuc = $"[{k.SW}{r[0].SW}] {k.S1W}-{r[0].S1W} {k.S2W}-{r[0].S2W} {k.S3W}-{r[0].S3W} {k.S4W}-{r[0].S4W} {k.S5W}-{r[0].S5W}";
+                dbl.Sonuc = $"[{k.SW}-{r[0].SW}] {k.S1W}-{r[0].S1W} {k.S2W}-{r[0].S2W} {k.S3W}-{r[0].S3W} {k.S4W}-{r[0].S4W} {k.S5W}-{r[0].S5W}";
 
                 if (k.SW > r[0].SW)
                 {
                     TMDW++;
                     TSDW += k.SW;
                 }
-                else
+                else if (k.SW < r[0].SW)
                 {
                     TMDL++;
                     TSDL += r[0].SW;
