11a59e9c31c5a15e5f38e022dfb15422556498d0
SenerDemiral <sener.demiral@gmail.com>
2017-11-02 00:58:39 +0300

    171102

--- bodved/Program.cs
+++ bodved/Program.cs
@@ -55,47 +55,6 @@ namespace bodved
 
             BDB.H.Write2Log("Start");
 
-            Handle.GET("/bodved/LoadPP", () =>
-            {
-                BDB.H.LoadPP();
-                return "OK";
-            });
-            Handle.GET("/bodved/LoadCC", () =>
-            {
-                BDB.H.LoadCC();
-                return "OK";
-            });
-            Handle.GET("/bodved/RestoreCC/{?}", (string ccID) =>
-            {
-                BDB.H.RestoreCC(ccID);
-                return "OK";
-            });
-
-
-            Handle.GET("/bodved/init", () =>
-            {
-                //popTable();
-                BDB.Tanimlar.popTable();
-                return "Init: OK";
-            });
-
-            Handle.GET("/bodved/deneme1", () =>
-            {
-                return BDB.Tanimlar.deneme1();
-            });
-            Handle.GET("/bodved/deneme2", () =>
-            {
-                return BDB.Tanimlar.deneme2();
-            });
-            Handle.GET("/bodved/deneme3", () =>
-            {
-                return BDB.Tanimlar.deneme3();
-            });
-            Handle.GET("/bodved/deneme4", () =>
-            {
-                return BDB.Tanimlar.deneme4();
-            });
-
             Handle.GET("/bodved/IndexCreate", () =>
             {
                 if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPP_Ad").FirstOrDefault() == null)
---
+++ scExpert/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using Starcounter;
+using BDB;
+
+namespace scExpert
+{
+    class Program
+    {
+        static void Main()
+        {
+            Handle.GET("/bodved/LoadPP", () =>
+            {
+                BDB.H.LoadPP();
+                return "OK";
+            });
+            Handle.GET("/bodved/LoadCC", () =>
+            {
+                BDB.H.LoadCC();
+                return "OK";
+            });
+            Handle.GET("/bodved/RestoreCC/{?}", (string ccID) =>
+            {
+                BDB.H.RestoreCC(ccID);
+                return "OK";
+            });
+
+
+            Handle.GET("/bodved/init", () =>
+            {
+                //popTable();
+                BDB.Tanimlar.popTable();
+                return "Init: OK";
+            });
+
+            Handle.GET("/scExpert/delCETP382", () =>
+            {
+                Db.Transact(() =>
+                {
+                    Db.SQL("DELETE FROM CETP where CET.ObjectNo = ?", 382);
+                });
+
+                return "OK";
+            });
+
+            Handle.GET("/bodved/deneme2", () =>
+            {
+                return BDB.Tanimlar.deneme2();
+            });
+            Handle.GET("/bodved/deneme3", () =>
+            {
+                return BDB.Tanimlar.deneme3();
+            });
+            Handle.GET("/bodved/deneme4", () =>
+            {
+                return BDB.Tanimlar.deneme4();
+            });
+
+
+        }
+    }
+}
