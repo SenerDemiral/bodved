ee621c93594fd7b24b6d95db3926b970b9fc7c92
SenerDemiral <sener.demiral@gmail.com>
2018-02-14 22:25:35 +0300

    180214

--- scExpert/Program.cs
+++ scExpert/Program.cs
@@ -115,24 +115,6 @@ namespace scExpert
                 return "OK: SaveCC()";
             });
 
-            Handle.GET("/scExpert/LoadPP", () =>
-            {
-                BDB.H.LoadPP();
-                return "OK: LoadPP()";
-            });
-
-            Handle.GET("/scExpert/LoadCC", () =>
-            {
-                BDB.H.LoadCC();
-                return "OK: LoadCC()";
-            });
-
-            Handle.GET("/scExpert/RestoreCC/{?}", (string ccID) =>
-            {
-                BDB.H.RestoreCC(ccID);
-                return $"OK: RestoreCC({ccID})";
-            });
-
             Handle.GET("/scExpert/BackupCC/{?}", (ulong CCoNo) =>
             {
                 BDB.H.BackupCC(CCoNo);
@@ -195,98 +177,15 @@ namespace scExpert
                 return BDB.Tanimlar.deneme4();
             });
 
-            Handle.GET("/scExpert/updPPsum", () =>
-            {
-                BDB.H.updPPsum();
-                return "updPPsum: OK";
-            });
-
             Handle.GET("/scExpert/IndexCreate", () =>
             {
-                
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPP_Ad").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxPP_Ad      ON BDB.PP (Ad)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPP_Rnk").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxPP_Rnk     ON BDB.PP (Rnk DESC)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPP_Sra").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxPP_Sra     ON BDB.PP (Sra)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPPGR_PPRnkID").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxPPGR_PPRnkID  ON BDB.PPGR (PP, RnkID)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPPGR_Rnk").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxPPGR_Rnk     ON BDB.PPGR (Rnk DESC)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxPPGR_RnkID").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxPPGR_RnkID   ON BDB.PPGR (RnkID)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCC_Idx").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCC_Idx     ON BDB.CC (Idx DESC)");
-                
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCT_CC").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCT_CC      ON BDB.CT (CC)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCET_CC_Trh").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCET_CC_Trh ON BDB.CET (CC, Trh)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCET_hCT").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCET_hCT    ON BDB.CET (hCT)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCET_gCT").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCET_gCT    ON BDB.CET (gCT)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCTP_CC").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCTP_CC     ON BDB.CTP (CC)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCTP_CT").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCTP_CT     ON BDB.CTP (CT)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCETP_CET").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCETP_CET   ON BDB.CETP (CET)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCETR_CC").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCETR_CC    ON BDB.CETR (CC)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCETR_CET").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCETR_CET   ON BDB.CETR (CET)");
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxCETR_RH").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxCETR_RH    ON BDB.CETR (RH)");
-
-                if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxRH_Trh").FirstOrDefault() == null)
-                    Db.SQL("CREATE INDEX IdxRH_Trh     ON BDB.RH (Trh)");
-                //if (Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", "IdxRH_CC").FirstOrDefault() == null)
-                //    Db.SQL("CREATE INDEX IdxRH_CC      ON BDB.RH (CC)");
-
+                H.IndexCreate();
                 return "IndexCreate: OK";
             });
 
             Handle.GET("/scExpert/IndexDrop", () =>
             {
-                Db.SQL("DROP INDEX IdxPP_Ad       ON BDB.PP");
-                Db.SQL("DROP INDEX IdxPP_Rnk      ON BDB.PP");
-                Db.SQL("DROP INDEX IdxPP_Sra      ON BDB.PP");
-
-                Db.SQL("DROP INDEX IdxPPGR_PPRnkID ON BDB.PPGR");
-                Db.SQL("DROP INDEX IdxPPGR_Rnk     ON BDB.PPGR");
-                Db.SQL("DROP INDEX IdxPPGR_RnkID   ON BDB.PPGR");
-
-                Db.SQL("DROP INDEX IdxRH_Trh      ON BDB.RH");
-                //Db.SQL("DROP INDEX IdxRH_CC       ON BDB.RH");
-
-                //Db.SQL("DROP INDEX IdxPRH_Trh     ON BDB.PRH");
-                //Db.SQL("DROP INDEX IdxPRH_PP_Trh  ON BDB.PRH");
-
-                Db.SQL("DROP INDEX IdxCC_Idx      ON BDB.CC");
-
-                Db.SQL("DROP INDEX IdxCT_CC       ON BDB.CT");
-
-                Db.SQL("DROP INDEX IdxCET_CC_Trh  ON BDB.CET");
-                Db.SQL("DROP INDEX IdxCET_hCT     ON BDB.CET");
-                Db.SQL("DROP INDEX IdxCET_gCT     ON BDB.CET");
-
-                Db.SQL("DROP INDEX IdxCTP_CC      ON BDB.CTP");
-                Db.SQL("DROP INDEX IdxCTP_CT      ON BDB.CTP");
-
-                Db.SQL("DROP INDEX IdxCETP_CET    ON BDB.CETP");
-
-                Db.SQL("DROP INDEX IdxCETR_CC     ON BDB.CETR");
-                Db.SQL("DROP INDEX IdxCETR_CET    ON BDB.CETR");
-                Db.SQL("DROP INDEX IdxCETR_RH     ON BDB.CETR");
-
+                H.IndexDrop();
                 return "IndexDrop: OK";
             });
 
